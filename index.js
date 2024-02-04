2233c073e696f05005d812393e290f672e3a4077
XUHT0730 <roy1108730@gmail.com>
2024-02-04 16:05:24 +0800

    add to git

---
+++ index.js
@@ -0,0 +1,177 @@
+const { createApp } = Vue;
+import userProductModal from "./userProductModal.js";
+// 3.定義規則，全部加入(CDN 版本)
+Object.keys(VeeValidateRules).forEach((rule) => {
+  if (rule !== "default") {
+    VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+  }
+});
+// 4. 設定語言環境
+VeeValidateI18n.loadLocaleFromURL(
+  "https://unpkg.com/@vee-validate/i18n@4.12.4/dist/locale/zh_TW.json"
+);
+// 使用 VeeValidate.configure 將回饋訊息的語言設定為繁體中文，並且在輸入內容時就即時驗證
+VeeValidate.configure({
+  generateMessage: VeeValidateI18n.localize("zh_TW"),
+  validateOnInput: true,
+});
+
+const apiUrl = "https://vue3-course-api.hexschool.io/v2";
+const apiPath = "hedy-api-path";
+const app = createApp({
+  data() {
+    return {
+      //處理 loading 載入效果
+      loadingStatus: {
+        loadingItem: "",
+      },
+      form: {
+        user: {
+          name: "",
+          email: "",
+          tel: "",
+          address: "",
+        },
+        message: "",
+      },
+      products: [],
+      product: {},
+      cart: {},
+    };
+  },
+  methods: {
+    getProducts() {
+      const getProductsUrl = `${apiUrl}/api/${apiPath}/products/all`;
+      axios
+        .get(getProductsUrl)
+        .then((res) => {
+          this.products = res.data.products;
+          this.isLoading = false;
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
+    getProduct(id) {
+      const getProductUrl = `${apiUrl}/api/${apiPath}/product/${id}`;
+      // 當執行取得產品函式時，將 id 設定到 loadingItem 中，
+      // 當 loadingItem 有值的情況會被自動轉型為 true，而 true 的時候畫面就會顯示轉圈圈動畫
+      this.loadingStatus.loadingItem = id;
+      axios
+        .get(getProductUrl)
+        .then((res) => {
+          // 當 axios 串接完成後再將 loadingItem 改回空字串，移除轉圈圈動畫，以此實現 loading 載入效果
+          this.loadingStatus.loadingItem = "";
+          this.product = res.data.product;
+          this.$refs.pModal.openModal();
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
+    getCart() {
+      const getCartUrl = `${apiUrl}/api/${apiPath}/cart`;
+      axios
+        .get(getCartUrl)
+        .then((res) => {
+          this.cart = res.data.data;
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
+    addToCart(id, qty = 1) {
+      const addToCartUrl = `${apiUrl}/api/${apiPath}/cart`;
+      this.loadingStatus.loadingItem = id;
+      const cart = {
+        product_id: id,
+        qty,
+      };
+      // 關閉 userProductModal，若使用者是從 userProductModal 中加入購物車，加上這段才能關閉 modal
+      this.$refs.pModal.closeModal();
+      axios
+        .post(addToCartUrl, { data: cart })
+        .then((res) => {
+          alert(res.data.message);
+          this.loadingStatus.loadingItem = "";
+          this.getCart();
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
+    updateCart(data) {
+      this.loadingStatus.loadingItem = data.id;
+      const updateCartUrl = `${apiUrl}/api/${apiPath}/cart/${data.id}`;
+      const cart = {
+        product_id: data.product_id,
+        qty: data.qty,
+      };
+      axios
+        .put(updateCartUrl, { data: cart })
+        .then((res) => {
+          alert(res.data.message);
+          this.loadingStatus.loadingItem = "";
+          this.getCart();
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+          this.loadingStatus.loadingItem = "";
+        });
+    },
+    deleteCartItem(id) {
+      const deleteCartItemUrl = `${apiUrl}/api/${apiPath}/cart/${id}`;
+      this.loadingStatus.loadingItem = id;
+      axios
+        .delete(deleteCartItemUrl)
+        .then((res) => {
+          alert(res.data.message);
+          this.loadingStatus.loadingItem = "";
+          this.getCart();
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
+    createOrder() {
+      const createOrderUrl = `${apiUrl}/api/${apiPath}/order`;
+      const order = this.form;
+      axios
+        .post(createOrderUrl, { data: order })
+        .then((res) => {
+          alert(res.data.message);
+          // 在成功後使用 resetForm() 語法來清除表單欄位的內容
+          // https://vee-validate.logaretm.com/v4/api/form#props:~:text=native%20form%20elements.-,resetForm,-%3A%20(state%3F%3A%20Partial%3CFormState
+          this.$refs.form.resetForm();
+          this.getCart();
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
+    deleteAllCartItems() {
+      const deleteAllCartItemsUrl = `${apiUrl}/api/${apiPath}/carts`;
+      axios
+        .delete(deleteAllCartItemsUrl)
+        .then((res) => {
+          alert(res.data.message);
+          this.getCart();
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
+  },
+  mounted() {
+    this.getProducts();
+    this.getCart();
+  },
+  components: {
+    userProductModal,
+  },
+});
+// 2. 註冊表單驗證元件
+app.component("VForm", VeeValidate.Form);
+app.component("VField", VeeValidate.Field);
+app.component("ErrorMessage", VeeValidate.ErrorMessage);
+app.mount("#app");
---
+++ userProductModal.js
@@ -0,0 +1,85 @@
+export default {
+  props: ["product", "addToCart"],
+
+  data() {
+    return {
+      userProductModal: null,
+      status: {},
+      qty: 1,
+    };
+  },
+  template: `<div
+    class="modal fade"
+    id="userProductProductModal"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+    ref="userProductModal"
+  >
+    <div class="modal-dialog modal-xl" role="document">
+      <div class="modal-content border-0">
+        <div class="modal-header bg-dark text-white">
+          <h5 class="modal-title" id="exampleModalLabel">
+            <span>{{ product.title }}</span>
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-sm-6">
+              <img class="img-fluid" :src="product.imagesUrl" alt="">
+            </div>
+            <div class="col-sm-6">
+              <span class="badge bg-primary rounded-pill">{{ product.category }}</span>
+              <p>商品描述：{{ product.description }}</p>
+              <p>商品內容：{{ product.content }}</p>
+              <div class="h5" v-if="product.origin_price === product.price">{{ product.price }} 元</div>
+              <div v-else>
+                <del class="h6"> 原價 {{ product.origin_price }} 元</del>
+                <div class="h5"> 現在只要 {{ product.price }} 元</div>
+              </div>
+              <div>
+                <div class="input-group">
+                  <input type="number" class="form-control" min="1"
+                   v-model.number="qty" />
+                  <button type="button" class="btn btn-primary" @click="addToCart(product.id, qty)">
+                    加入購物車
+                  </button>
+                </div>
+              </div>
+            </div>
+            <!-- col-sm-6 end -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`,
+  methods: {
+    openModal() {
+      this.userProductModal.show();
+    },
+    closeModal() {
+      this.userProductModal.hide();
+    },
+  },
+  // 每當 product 的 qty 有變動時，下次開 Modal 時自動條回 1
+  watch: {
+    product() {
+      this.qty = 1;
+    },
+  },
+  mounted() {
+    this.userProductModal = new bootstrap.Modal(this.$refs.userProductModal, {
+      // 禁止使用者使用 ESC 鍵關閉互動視窗
+      keyboard: false,
+      // 禁止使用者點擊 modal 以外的地方來關閉視窗，避免資料輸入到一半遺失
+      backdrop: "static",
+    });
+  },
+};
